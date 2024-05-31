f7d57f163ac0127f9102d1a865cc12900078621a
G4meD3v <mrwpgg@gmail.com>
2024-05-31 11:56:55 +0330

    add base class for wheel data

--- Assets/Script/Vehicle/VehicleMovementComponent.cs
+++ Assets/Script/Vehicle/VehicleMovementComponent.cs
@@ -5,23 +5,63 @@ using UnityEngine;
 
 public class VehicleMovementComponent : MonoBehaviour
 {
+    [System.Serializable]
+    public struct WheelDescription
+    {
+        public string boneName;
+        public WheelData wheelData;
+    }
+
+    private struct WheelDataInternal
+    {
+        public string BoneName;
+        public float WheelWidth;
+        public float WheelRadius;   
+    };
+
     private Rigidbody RB;
     private Vector3 GravityDirection;
+
+    [SerializeField]
+    public WheelDescription[] WheelsDescription;
+
+    // ------------------------------------------------------------------------------------------------------------
 
     void Start()
     {
         RB = GetComponent<Rigidbody>();
 
         GravityDirection = Vector3.down;
+
+        InitWheels();
     }
 
     
 
     void FixedUpdate()
     {
+        // Gravity
         Vector3 GravityForce = GravityDirection * Physics.gravity.magnitude * Time.fixedDeltaTime;
         RB.AddForce(GravityForce.x, GravityForce.y, GravityForce.z, ForceMode.VelocityChange);
 
 
+    }
+
+    void InitWheels()
+    {
+        foreach (WheelDescription WheelDesc in WheelsDescription)
+        {
+            if (WheelDesc.wheelData)
+            {
+                WheelDataInternal WDI;
+                WDI.BoneName = WheelDesc.boneName;
+                WDI.WheelWidth = WheelDesc.wheelData.WheelWidth;
+                WDI.WheelRadius = WheelDesc.wheelData.WheelRadius;
+            }
+            else 
+            {
+                Debug.LogError(name + ": missing WheelData!");
+            }
+        }
     }
 }
---
+++ Assets/Script/Vehicle/WheelData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WheelData : ScriptableObject
+{
+    public float WheelWidth = 10.0f;
+    public float WheelRadius = 10.0f;
+
+
+}
