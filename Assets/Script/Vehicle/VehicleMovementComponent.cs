e4ccdce6fdb2be97fb498175fec11ae23baa2171
G4meD3v <mrwpgg@gmail.com>
2024-06-02 13:31:19 +0330

    add basic anti roll

--- Assets/Script/Vehicle/VehicleMovementComponent.cs
+++ Assets/Script/Vehicle/VehicleMovementComponent.cs
@@ -17,15 +17,28 @@ public class VehicleMovementComponent : MonoBehaviour
     public float steeringRange = 30;
     public float steeringRangeAtMaxSpeed = 10;
     public float centreOfMassOffset = -1f;
+    public float AntiRoll= 5000.0f;
 
-    VehicleWheel[] wheels;
+    VehicleWheel[] wheels = new VehicleWheel[4];
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.centerOfMass += Vector3.up * centreOfMassOffset;
 
-        wheels = GetComponentsInChildren<VehicleWheel>();
+        VehicleWheel[] allWheels = GetComponentsInChildren<VehicleWheel>();
+
+        foreach (VehicleWheel wheel in allWheels) 
+        {
+            if (wheel.name == "Wheel_FL")
+                wheels[0] = wheel;
+            else if (wheel.name == "Wheel_FR")
+                wheels[1] = wheel;
+            else if (wheel.name == "Wheel_RL")
+                wheels[2] = wheel;
+            else if (wheel.name == "Wheel_RR")
+                wheels[3] = wheel;
+        }
 
         gravityDirection = Vector3.down;
 
@@ -37,6 +50,8 @@ public class VehicleMovementComponent : MonoBehaviour
         Vector3 GravityForce = gravityDirection * Physics.gravity.magnitude * Time.fixedDeltaTime;
         rb.AddForce(GravityForce.x, GravityForce.y, GravityForce.z, ForceMode.VelocityChange);
 
+        AntiRollForAxis(wheels[0].WheelCollider, wheels[1].WheelCollider);
+        AntiRollForAxis(wheels[2].WheelCollider, wheels[3].WheelCollider);
     }
 
     private void Update()
@@ -50,6 +65,12 @@ public class VehicleMovementComponent : MonoBehaviour
         float currentSteerRange = Mathf.Lerp(steeringRange, steeringRangeAtMaxSpeed, speedFactor);
 
         bool isAccelerating = Mathf.Sign(vInput) == Mathf.Sign(forwardSpeed);
+
+        DrawHelpers.DrawSphere(rb.worldCenterOfMass, 1, Color.green);
+
+
+
+
 
         foreach (var wheel in wheels)
         {
@@ -74,4 +95,32 @@ public class VehicleMovementComponent : MonoBehaviour
         }
     }
 
+
+    void AntiRollForAxis(WheelCollider L, WheelCollider R)
+    {
+        WheelHit hit;
+
+        float travelL = 1.0f;
+        float travelR = 1.0f;
+
+        var groundedL = L.GetGroundHit(out hit);
+        if (groundedL)
+        { 
+            travelL = (-L.transform.InverseTransformPoint(hit.point).y - L.radius) / L.suspensionDistance;
+        }
+
+        var groundedR = R.GetGroundHit(out hit);
+        if (groundedR)
+        {
+            travelL = (-R.transform.InverseTransformPoint(hit.point).y - R.radius) / R.suspensionDistance;
+        }
+
+        var antiRollForce = (travelL - travelR) * AntiRoll;
+
+        if (groundedL)
+            rb.AddForceAtPosition(L.transform.up * -antiRollForce, L.transform.position);
+        if (groundedR)
+            rb.AddForceAtPosition(R.transform.up * antiRollForce, R.transform.position);
+
+    }
 }
