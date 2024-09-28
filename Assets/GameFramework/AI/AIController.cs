bcee8e86bc552170299c1e26e6146e69f2d7b6e1
G4meD3v <mrwpgg@gmail.com>
2024-09-28 13:11:02 +0330

    update

--- Assets/GameFramework/AI/AIController.cs
+++ Assets/GameFramework/AI/AIController.cs
@@ -4,11 +4,15 @@ public class AIController : MonoBehaviour
 {
     public Vehicle vehicle;
 
+    public AI_Route_Node aiRouteNode_Previous;
     public AI_Route_Node aiRouteNode_Current;
     public AI_Route_Node aiRouteNode_Target;
 
+    public float projectionDistance = 3.0f;
+
     public void OnEnterNewRouteNode(AI_Route_Node node)
     {
+        aiRouteNode_Previous = aiRouteNode_Current;
         aiRouteNode_Current = node;
 
         UpdateTargetNode();
@@ -27,6 +31,54 @@ public class AIController : MonoBehaviour
         }
     }
 
+    public Vector3 GetNearestWorldPosition()
+    {
+        if (aiRouteNode_Current && aiRouteNode_Target)
+        {
+            Vector3 d_n = aiRouteNode_Target.transform.position - aiRouteNode_Current.transform.position;
+            Vector3 toPos_n = vehicle.vehicleProxy.transform.position - aiRouteNode_Current.transform.position;
+
+            float dot_n = Vector3.Dot(d_n.normalized, toPos_n);
+
+            Debug.Log(dot_n);
+
+            // we passed target without hitting collision
+            if (dot_n > d_n.magnitude)
+            {
+                OnEnterNewRouteNode(aiRouteNode_Target);
+
+                return GetNearestWorldPosition();
+            }
+
+            else if (dot_n < 0 && aiRouteNode_Previous)
+            {
+                Vector3 d_p = aiRouteNode_Current.transform.position - aiRouteNode_Previous.transform.position;
+                Vector3 toPos_p = vehicle.vehicleProxy.transform.position - aiRouteNode_Previous.transform.position;
+
+                float dot_p = Vector3.Dot(d_p.normalized, toPos_p);
+                dot_p += projectionDistance;
+
+                float remaining = d_p.magnitude - dot_p;
+                if (remaining > 0)
+                {
+                    return aiRouteNode_Previous.transform.position + dot_p * d_p.normalized;
+                }
+                else
+                {
+                    d_p = aiRouteNode_Target.transform.position - aiRouteNode_Current.transform.position;
+                    return aiRouteNode_Current.transform.position - remaining * d_p.normalized;
+                }
+            }
+
+            else
+            {
+                return aiRouteNode_Current.transform.position + (dot_n + projectionDistance) * d_n.normalized;
+            }
+        }
+
+        return Vector3.zero;
+    }
+
     void Start()
     {
         vehicle = GetComponent<Vehicle>();
@@ -39,11 +91,7 @@ public class AIController : MonoBehaviour
             vehicle.vInput = 1;
         }
 
-        if(aiRouteNode_Current && aiRouteNode_Target)
-        {
-            DrawHelpers.DrawSphere(aiRouteNode_Target.transform.position, 5, Color.blue);
-        }
+        DrawHelpers.DrawSphere(GetNearestWorldPosition(), 5, Color.blue);
     }
-
 
 }
