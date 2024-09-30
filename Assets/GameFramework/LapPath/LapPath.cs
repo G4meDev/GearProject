669a9a297016c612656cab405b0383ca8c75ad60
G4meD3v <mrwpgg@gmail.com>
2024-09-30 23:09:14 +0330

    update

---
+++ Assets/GameFramework/LapPath/LapPath.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class LapPath : MonoBehaviour
+{
+    
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(LapPath))]
+public class LapPath_Editor : Editor
+{
+    public bool cDown = false;
+
+    private void OnEnable()
+    {
+        SceneView.duringSceneGui += CustomOnSceneGUI;
+    }
+
+//     private void OnDisable()
+//     {
+//         SceneView.duringSceneGui -= CustomOnSceneGUI;
+//     }
+
+    private void CustomOnSceneGUI(SceneView view)
+    {
+        var t = target as LapPath;
+        if (!t)
+            return;
+
+        LapPath_Node[] nodes = t.GetComponentsInChildren<LapPath_Node>();
+
+//         foreach (AI_Route_Node node in nodes)
+//         {
+//             foreach (AI_Route_Node child in node.children)
+//             {
+//                 if (child)
+//                 {
+//                     Vector3 halfPos = (node.transform.position + child.transform.position) / 2;
+//                     Handles.DrawDottedLine(node.transform.position, halfPos, 5);
+//                     Handles.DrawLine(halfPos, child.transform.position, 10);
+//                 }
+//             }
+// 
+//             foreach(AI_Route_Node parent in node.parents)
+//             {
+//                 if(parent)
+//                 {
+//                     Vector3 offset = Vector3.up * 5;
+// 
+//                     Vector3 halfPos = (node.transform.position + parent.transform.position) / 2;
+//                     Handles.DrawDottedLine(node.transform.position + offset, halfPos + offset, 5);
+//                     Handles.DrawLine(halfPos + offset, parent.transform.position + offset, 10);
+//                 }
+//             }
+//         }
+
+        //---------------------------------------------------------------------
+
+        Event e = Event.current;
+        if (e.type == EventType.KeyDown && e.keyCode == KeyCode.C && !cDown)
+        {
+            cDown = true;
+
+            OnConnect();
+        }
+
+        else if (e.type == EventType.KeyUp && e.keyCode == KeyCode.C)
+        {
+            cDown = false;
+        }
+
+    }
+
+    private void OnConnect()
+    {
+        GameObject[] objs = Selection.gameObjects;
+
+        if(objs.Length != 2)
+        {
+            return;
+        }
+
+        LapPath_Node n1 = objs[0].GetComponent<LapPath_Node>();
+        LapPath_Node n2 = objs[1].GetComponent<LapPath_Node>();
+
+        if(!n1 || !n2)
+        {
+            return;
+        }
+
+        if (!n1.children.Contains(n2))
+            (n1, n2) = (n2, n1);
+
+        n1.children.Remove(n2);
+        n2.parents.Remove(n1);
+
+        if (!n1.parents.Contains(n2))
+            n1.parents.Add(n2);
+
+        if (!n2.children.Contains(n1))
+            n2.children.Add(n1);
+
+        n1.OnNeighboursChanged();
+        n1.OnNeighboursChanged();
+
+        EditorUtility.SetDirty(n1);
+        EditorUtility.SetDirty(n2);
+
+//         foreach (GameObject obj in objs)
+//         {
+//             Debug.Log(obj.name);
+//         }
+    }
+}
+
+#endif
---
+++ Assets/GameFramework/LapPath/LapPath_Node.cs
@@ -0,0 +1,165 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
+
+public class LapPath_Node : MonoBehaviour
+{
+    public List<LapPath_Node> children;
+    public List<LapPath_Node> parents;
+
+    public GameObject spawnPoint;
+
+    public float nodeIndex = 0;
+
+    Vector3 GetBoxCorner(Vector3 localPos)
+    {
+        return transform.TransformPoint(localPos / 2);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 topNorthEast = GetBoxCorner(new(1, 1, 1));
+        Vector3 topSouthWest = GetBoxCorner(new(-1, 1, -1));
+
+        foreach (LapPath_Node child in children)
+        {
+            if (child)
+            {
+                Vector3 childTopSouthEast = child.GetBoxCorner(new(1, 1, -1));
+            
+                Vector3 dir = childTopSouthEast - topNorthEast;
+                DrawArrow.ForGizmo(topNorthEast, dir, Color.red, 5);
+            }
+        }
+
+        foreach (LapPath_Node parent in parents)
+        {
+            if (parent)
+            {
+                Vector3 parentTopNorthWest = parent.GetBoxCorner(new(-1, 1, 1));
+
+                Vector3 dir = parentTopNorthWest - topSouthWest;
+                DrawArrow.ForGizmo(topSouthWest, dir, Color.blue, 5);
+            }
+        }
+
+    }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         Vehicle vehicle = other.transform.root.GetComponentInChildren<Vehicle>();
+// 
+//         AIController controller = vehicle.GetComponent<AIController>();
+//         if (controller)
+//         {
+//             controller.OnEnterNewRouteNode(this);
+//         }
+//     }
+
+    public void OnNeighboursChanged()
+    {
+
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(this);
+#endif
+    }
+
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(LapPath_Node))]
+[CanEditMultipleObjects]
+public class LapPath_Node_Editor : Editor
+{
+    public bool bDown = false;
+    public bool vDown = false;
+
+    void OnSceneGUI()
+    {
+        //GameObject obj = target as GameObject;
+
+        Event e = Event.current;
+
+        if (e.type == EventType.KeyDown && e.keyCode == KeyCode.B && !bDown)
+        {
+            bDown = true;
+            Extrude();
+        }
+
+        else if (e.type == EventType.KeyUp && e.keyCode == KeyCode.B)
+        {
+            bDown = false;
+        }
+
+        // -----------------------------------------------------------
+
+        else if (e.type == EventType.KeyDown && e.keyCode == KeyCode.V && !bDown)
+        {
+            vDown = true;
+            Align();
+        }
+
+        else if (e.type == EventType.KeyUp && e.keyCode == KeyCode.V)
+        {
+            vDown = false;
+        }
+    }
+
+    private void Extrude()
+    {
+        LapPath_Node node = target as LapPath_Node;
+
+        Object obj = PrefabUtility.InstantiatePrefab(node.GetPrefabDefinition());
+
+        LapPath_Node newNode = obj.GetComponent<LapPath_Node>();
+        newNode.transform.parent = node.transform.root;
+
+        newNode.transform.SetPositionAndRotation(node.transform.position + node.transform.forward * 50, node.transform.rotation);
+        newNode.transform.localScale = node.transform.localScale;
+
+        int ix = node.name.LastIndexOf('_');
+
+        string s = node.name.Substring(ix + 1, node.name.Length - ix - 1);
+        int i = int.Parse(s) + 1;
+
+        s = node.name[..(ix + 1)] + i;
+        newNode.name = s;
+        
+        newNode.children.Clear();
+        newNode.parents.Clear();
+        newNode.parents.Add(node);
+
+        node.children.Add(newNode);
+
+        Selection.activeObject = newNode;
+
+        LapPath_Node[] nodes = node.transform.root.GetComponentsInChildren<LapPath_Node>();
+        foreach(LapPath_Node n in nodes)
+        {
+            n.OnNeighboursChanged();
+        }
+        
+    }
+
+    private void Align()
+    {
+        Debug.Log("aa");
+
+        LapPath_Node node = target as LapPath_Node;
+
+        Ray ray = new(node.transform.position, -node.transform.up);
+        bool bHit = Physics.Raycast(ray, out RaycastHit hit);
+
+        if (bHit)
+        {
+            Vector3 forward = Vector3.Cross(node.transform.right, hit.normal);
+            node.transform.rotation = Quaternion.LookRotation(forward, hit.normal);
+            node.transform.position = hit.point;
+            EditorUtility.SetDirty(node);
+        }
+    }
+}
+
+#endif
